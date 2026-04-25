0926485bd9e827e70200a064508c7c5678a850d2
Jelmer Lipperts <jelmer@lippertsmedia.com>
2026-04-25 12:49:30 +0200

    Initial Heijmans Techniek site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://heijmanstechniek.nl",
+  output: "static",
+  adapter: cloudflare(),
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
